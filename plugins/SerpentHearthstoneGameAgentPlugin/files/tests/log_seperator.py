d092c9200696c1d7f3e704cc1f17240e1301d784
Zaibo Wang <zaibowang@gmail.com>
2018-01-17 10:38:13 -0500

    Initial log seperator

--- plugins/SerpentHearthstoneGameAgentPlugin/files/tests/log_seperator.py
+++ plugins/SerpentHearthstoneGameAgentPlugin/files/tests/log_seperator.py
@@ -6,4 +6,11 @@ class LogSeperator():
     match = "CREATE_GAME"
     def __init__(self, file):
         with io.open(file) as logs:
-            pass
+            k = 0
+            for line in logs.readlines():
+                if "GameState.DebugPrintPower() - CREATE_GAME" in line:
+                    print(line)
+                    k += 1
+            print(k)
+
+a = LogSeperator('/home/zaibo/code/Hearthstone-Quest-Bot/plugins/SerpentHearthstoneGameAgentPlugin/files/tests/many_games.log')
